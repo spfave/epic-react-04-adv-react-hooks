59f842d06e4fb6cd43bba9a410abe266ba779118
Sebastian F <sebpfdev@gmail.com>
2022-04-10 16:13:13 -0400

    Complete exercise 3 extra credit 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,6 +4,7 @@
 import * as React from 'react'
 
 // 🐨 create your CountContext here with React.createContext
+// Base solution
 const CountContext = React.createContext()
 
 // 🐨 create a CountProvider component here that does this:
@@ -15,23 +16,38 @@ function CountProvider(props) {
   const [count, setCount] = React.useState(0)
 
   return (
-    <CountContext.Provider value={{count, setCount}} {...props}>
+    <CountContext.Provider value={[count, setCount]} {...props}>
       {props.children}
     </CountContext.Provider>
   )
 }
 
+// Extra credit 1:Create a consumer hook to create and use the count context
+function useCount() {
+  const countContext = React.useContext(CountContext)
+
+  if (countContext == null) {
+    throw new Error(`useCount must be used within a CountProvider`)
+  }
+
+  return countContext
+}
+
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
   // const count = 0
-  const {count} = React.useContext(CountContext)
+  // const [count] = React.useContext(CountContext) // base
+  const [count] = useCount() // ec1
+
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
   // const setCount = () => {}
-  const {setCount} = React.useContext(CountContext)
+  // const [, setCount] = React.useContext(CountContext) // base
+  const [, setCount] = useCount() // ec1
+
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -39,10 +55,8 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
+      {/* 🐨 wrap these two components in the CountProvider so they can access
+        the CountContext value */}
       <CountProvider>
         <CountDisplay />
         <Counter />
--- survey-notes.md
+++ survey-notes.md
@@ -3,9 +3,9 @@
 ## 01: useReducer
 When managing an independent element of state using a stand alone state and state setter is advantageous  
 When one element of state relies on another element of state for updating using a reducer function is advantageous  
-Input to reducer function  
+Input to a reducer function  
 1. state: the current state
-2. action: value that that is passed to the updater function (dispatch function), alternately whatever is passed to the dispatch function will be th second argument to the reducer function  
+2. action: value that that is passed to the updater function (dispatch function), alternately whatever is passed to the dispatch function will be the second argument to the reducer function  
 
 Input to useReducer hook  
 1. reducer function
@@ -17,3 +17,14 @@ Memoization is a performance optimization technique which eliminates the need to
 The purpose of useMemo and useCallback is to memoize values and functions in dependency list and props on memoized components   
 
 ## 03: useContext
+The react context API allows for the creation of a store of data and/or functionality that can be accessed anywhere within the children of the context.  
+To create and use a React context the following APIs are used  
+1. createContext: to create the context store 
+2. useContext: React hook to access a context store from within a child component of a context
+
+Input to createContext  
+1. optional: default value for the context 
+
+Input to useContext  
+1. name of a context store
+
